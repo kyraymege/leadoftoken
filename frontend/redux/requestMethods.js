546ff8bf241270db04ca2c1a567681618f7a46b9
Ege Kayra <egekayra.business@gmail.com>
2022-07-28 21:34:49 +0300

    for deployment

--- frontend/redux/requestMethods.js
+++ frontend/redux/requestMethods.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:8800/api/";
+const BASE_URL = "http://18.156.166.199/api/";
 
 export const publicRequest = axios.create({
   withCredentials: true,
   baseURL: BASE_URL,
 });
 
-export const PF = "http://localhost:8800/tokenlogos/";
+export const PF = "http://18.156.166.199/tokenlogos/";
 
 
