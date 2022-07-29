585477f212db505c2cc3b288bbf0c9415c1c0d2b
Ege Kayra <egekayra.business@gmail.com>
2022-07-29 23:37:22 +0300

    fix

--- frontend/components/Auth/RegisterComponent.js
+++ frontend/components/Auth/RegisterComponent.js
@@ -43,7 +43,7 @@ export default function RegisterComponent() {
 
     return (        
         <>        
-            <div className="bg-indigo-50 dark:bg-gray-900 ">
+            <div className="bg-indigo-50 min-h-screen dark:bg-gray-900 ">
                 <div className="xl:px-20 md:px-10 sm:px-6 px-4 md:py-12 py-9 2xl:mx-auto 2xl:container md:flex items-center justify-center">
 
                     <div className="bg-white dark:bg-gray-800 shadow-lg rounded xl:w-1/3 lg:w-5/12 md:w-1/2 w-full lg:px-10 sm:px-6 sm:py-10 px-2 py-6">
