89b3dd5b4f6646d70c6e2c5481a6763339a5388b
Ege Kayra <egekayra.business@gmail.com>
2022-12-10 23:05:12 +0300

    ad

--- frontend/components/News/NewsDetails/Ad.js
+++ frontend/components/News/NewsDetails/Ad.js
@@ -6,7 +6,7 @@ const ad = () => {
     <div className='h-[900px] w-[300px]'>
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1438992172560378"
         crossorigin="anonymous"></Script>
-      <ins class="adsbygoogle"
+      <ins className="adsbygoogle"
         style="display:block"
         data-ad-client="ca-pub-1438992172560378"
         data-ad-slot="5438016209"
