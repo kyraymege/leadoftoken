b4b952912203a62e001c00cf5117f1b8eb22b6ec
Ege Kayra <egekayra.business@gmail.com>
2022-12-10 23:12:48 +0300

    ad

--- frontend/components/News/NewsDetails/Ad.js
+++ frontend/components/News/NewsDetails/Ad.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Script from 'next/script'
 
 const ad = () => {
+
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, [])
+
   return (
     <div className='h-[900px] w-[300px]'>
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1438992172560378"
@@ -12,9 +17,6 @@ const ad = () => {
         data-ad-slot="5438016209"
         data-ad-format="auto"
         data-full-width-responsive="true"></ins>
-      <Script id="google-adsense">
-        (adsbygoogle = window.adsbygoogle || []).push({ });
-      </Script>
     </div>
   )
 }
