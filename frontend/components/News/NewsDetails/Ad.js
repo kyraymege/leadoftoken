00a5fdfe129ad068d78b6dc9495d58c795f0f664
Ege Kayra <egekayra.business@gmail.com>
2022-12-10 23:01:30 +0300

    ad

--- frontend/components/News/NewsDetails/Ad.js
+++ frontend/components/News/NewsDetails/Ad.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Script from 'next/script'
 
 const ad = () => {
   return (
     <div className='h-[900px] w-[300px]'>
-        <img src='https://s0.2mdn.net/simgad/7418425147621968614' alt='ad' />
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1438992172560378"
+        crossorigin="anonymous"></Script>
+      <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-1438992172560378"
+        data-ad-slot="5438016209"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+      <Script>
+        (adsbygoogle = window.adsbygoogle || []).push({ });
+      </Script>
     </div>
   )
 }
